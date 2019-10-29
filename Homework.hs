edbaa0925d9886ccc6341eb6eb631f6c5f4395d4
dabobrov_19214 <55199362+19214bobrov@users.noreply.github.com>
2019-10-29 21:02:46 +0300

    Update Homework.hs

--- Homework.hs
+++ Homework.hs
@@ -1,10 +1,8 @@
-jam a b c 
-    |( a == 0 && b == 0) = error "NO ROOTS!"
-    |( a == 0 && b /= 0) = (x,x) 
-    |( discriminant < 0) = error "NO REAL ROOTS!"
-    |( a /= 0) = (x1, x2)
-        where
-            discriminant = b^2 - 4 * a * c
-            x1 = (-b + sqrt discriminant) / (2 * a)
-            x2 = (-b - sqrt discriminant) / (2 * a)
-            x = (-c / b)
+square 0 b c = ( -c / b, -c / b)
+square 0 0 _ = error "no roots"
+square a b c | discriminant < 0 = error "NO REAL ROOTS"
+                        | otherwise = (x1, x2)
+                        where
+                             discriminant = b * b - 4 * a * c
+                             x1 = (-b + sqrt discriminant) / (2 * a)
+                             x2 = (-b - sqrt discriminant) / (2 * a)

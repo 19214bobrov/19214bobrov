5793dc1a9a42d1940f2e75add5a240d1fb24de91
dabobrov_19214 <55199362+19214bobrov@users.noreply.github.com>
2019-11-23 18:49:35 +0300

    Add files via upload

---
+++ Haskell.Music.hs
@@ -0,0 +1,39 @@
+data Group = ToGroup {
+                      gpName :: String,
+                      gpMembers :: [String],
+                      gpSong :: [Song]
+                      } deriving Show
+
+
+data Album = ToAlbum {
+                      amName :: String,
+                      amSong :: [String]
+                      } deriving Show
+
+
+data Song = ToSong {
+                    sgName :: String
+                    } deriving Show
+
+
+data User = ToUser {
+                    urName :: String,
+                    urSongs :: [Song],
+                    urAlbums :: [Album],
+                    urGroups :: [Group]
+                    } deriving Show
+
+
+newGroup :: User -> Group -> User
+newGroup user group = ToUser (urName user) (urSongs user) (urAlbums user) (urGroups user ++ [group])
+
+
+newAlbum :: User -> Album -> User
+newAlbum user album = ToUser (urName user) (urSongs user) (urAlbums user ++ [album]) (urGroups user)
+
+
+newSong :: User -> Song -> User
+newSong user song = ToUser (urName user) (urSongs user ++ [song]) (urAlbums user) (urGroups user)
+
+
+searchSongByName user name = [song | song <- (urSongs user), sgName song == name]

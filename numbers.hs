b827a2ad78f60384484288a092fa14be9e1dd9a6
dabobrov_19214 <55199362+19214bobrov@users.noreply.github.com>
2019-12-17 23:50:36 +0300

    Update numbers.hs

--- numbers.hs
+++ numbers.hs
@@ -1,27 +1,34 @@
 import Data.Char
 import Data.List
 
-charToInteger :: Char -> Int
-charToInteger x | ord x >= 48 && ord x <= 57 = ord x - 48
-                | ord x >= 65 && ord x <= 90 = ord x - 29
-                | ord x >= 97 && ord x <= 122 = ord x - 87
+charToInteger :: Char -> Integer
+charToInteger x | (ord x) >= ord 'a' && (ord x) <= ord 'z' = toInteger $ ord x - ord 'a' + 10
+                | (ord x) >= ord '0' && (ord x) <= ord '9' = toInteger $ ord x - ord '0'
+                | (ord x) >= ord 'A' && (ord x) <= ord 'Z' = toInteger $ ord x - ord 'A' + 36
                 | otherwise = error "invalid char"
-integerToChar :: Int -> Char
-integerToChar x | x >= 0 && x <= 9 = chr (x + 48)
-                | x >= 10 && x <= 35 = chr (x + 87)
-                | x >= 36 && x <= 61 = chr (x + 29)
+integerToChar :: Integer -> Char
+integerToChar x | x >= 0 && x <= 9 = chr (fromInteger x + ord '0')
+                | x >= 10 && x <= 35 = chr (fromInteger x + ord 'a' - 10)
+                | x >= 36 && x <= 61 = chr (fromInteger x + ord 'A' - 36)
                 | otherwise = error "invalid integer"
-toDecimal :: Int -> String -> String
-toDecimal base snumber | base < 1 || base > 61 = error "wrong input"
-                       | otherwise = show $ f ((length snumber) - 1)(map charToInteger snumber) base
+toDecimal :: Integer -> String -> String
+toDecimal 1 snumber | all (=='1') snumber = show $ length snumber
+                    | otherwise = error "invalid character"
+toDecimal base snumber | base >= 1 && base <= 61 = show (answer acc $ length acc)
+                       | otherwise = error "base invalid"
                         where
-                             f n [] base = 0
-                             f n (x:xs) base = x * base ^ n + f (n - 1) xs base
-fromDecimal :: Int -> String -> String
-fromDecimal toBase snumber | toBase < 1 || toBase > 61 = error "wrong input"
-                           | otherwise = reverse (f (read snumber) toBase)
+                             acc = map charToInteger snumber
+                             answer [] _ = 0
+                             answer (a:acc) power | a < base = a * (base ^ (power - 1)) + answer acc (power - 1)
+fromDecimal :: Integer -> String -> String
+fromDecimal 1 snumber | any(\char -> ord char < 48 || ord char > 57) snumber = error "incorrect number"
+                      | otherwise = replicate (read snumber) '1'
+fromDecimal toBase snumber | any(\char -> ord char < 48 || ord char > 57) snumber = error "incorrect number"
+                           | toBase <= 61 && toBase > 1 = map integerToChar (answer number [])
+                           | otherwise = error "invalid base"
                             where
-                                 f 0 toBase = []
-                                 f number toBase = integerToChar ( number `mod` toBase) : (f (number `div` toBase) toBase)
-convertFromTo :: Int -> Int -> String -> String
+                                 number = read snumber
+                                 answer 0 acc = acc
+                                 answer number acc = (answer ( number `div` toBase) (number `mod` toBase : acc))
+convertFromTo :: Integer -> Integer -> String -> String
 convertFromTo fromBase toBase snumber = fromDecimal toBase $ toDecimal fromBase snumber

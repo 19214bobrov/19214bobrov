0db2d8bcc80ec38667b6e26f4b9decf6a84cb353
dabobrov_19214 <55199362+19214bobrov@users.noreply.github.com>
2019-12-18 11:42:55 +0300

    Update numbers.hs

--- numbers.hs
+++ numbers.hs
@@ -6,11 +6,13 @@ charToInteger x | (ord x) >= ord 'a' && (ord x) <= ord 'z' = toInteger $ ord x -
                 | (ord x) >= ord '0' && (ord x) <= ord '9' = toInteger $ ord x - ord '0'
                 | (ord x) >= ord 'A' && (ord x) <= ord 'Z' = toInteger $ ord x - ord 'A' + 36
                 | otherwise = error "invalid char"
+
 integerToChar :: Integer -> Char
-integerToChar x | x >= 0 && x <= 9 = chr (fromInteger x + ord '0')
-                | x >= 10 && x <= 35 = chr (fromInteger x + ord 'a' - 10)
-                | x >= 36 && x <= 61 = chr (fromInteger x + ord 'A' - 36)
+integerToChar x | x >= 0 && x <= 9 = chr (fromIntegral x + ord '0')
+                | x >= 10 && x <= 35 = chr (fromIntegral x + ord 'a' - 10)
+                | x >= 36 && x <= 61 = chr (fromIntegral x + ord 'A' - 36)
                 | otherwise = error "invalid integer"
+
 toDecimal :: Integer -> String -> String
 toDecimal 1 snumber | all (=='1') snumber = show $ length snumber
                     | otherwise = error "invalid character"
@@ -20,6 +22,7 @@ toDecimal base snumber | base >= 1 && base <= 61 = show (answer acc $ length acc
                              acc = map charToInteger snumber
                              answer [] _ = 0
                              answer (a:acc) power | a < base = a * (base ^ (power - 1)) + answer acc (power - 1)
+ 
 fromDecimal :: Integer -> String -> String
 fromDecimal 1 snumber | any(\char -> ord char < 48 || ord char > 57) snumber = error "incorrect number"
                       | otherwise = replicate (read snumber) '1'
@@ -30,5 +33,6 @@ fromDecimal toBase snumber | any(\char -> ord char < 48 || ord char > 57) snumbe
                                  number = read snumber
                                  answer 0 acc = acc
                                  answer number acc = (answer ( number `div` toBase) (number `mod` toBase : acc))
+
 convertFromTo :: Integer -> Integer -> String -> String
 convertFromTo fromBase toBase snumber = fromDecimal toBase $ toDecimal fromBase snumber
